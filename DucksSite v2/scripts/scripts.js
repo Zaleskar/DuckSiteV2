e234e3cdbe2b7b1d433bff54d04f43f2725b49cf
Zaleskar <106952045+Zaleskar@users.noreply.github.com>
2024-03-15 11:18:48 +0300

    Add files via upload

---
+++ DucksSite v2/scripts/scripts.js
@@ -0,0 +1,19 @@
+let textGradient = document.getElementById('header1');
+let BigBoy = document.getElementById('big_boy');
+let navbar = document.getElementById('navbar')
+let navbarOutline = document.getElementById('navbarOutline');
+
+
+
+
+window.addEventListener("load", () => {
+    textGradient.style.scale = "95%";
+    BigBoy.style.scale = "105%";
+})
+
+
+
+
+
+
+
